b228b73b48a5bfccf9c862e675daf94f8a1df61b
CollinsKippy <115981895+CollinsKippy@users.noreply.github.com>
2023-02-02 13:14:44 +0300

    1 file modified removed axios

--- features/products/product.thunks.tsx
+++ features/products/product.thunks.tsx
@@ -1,5 +1,4 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 import { BASE_URL } from '../../app/constants';
 // import { loadProductsFailed, loadProductsSuccess } from './product.actions';
 
@@ -8,8 +7,15 @@ const productsUrl = BASE_URL + '/products';
 export const fetchProducts = createAsyncThunk(
   '[PRODUCTS API] Load Products',
   async () => {
-    const { data } = await axios.get(productsUrl);
+    const res = await fetch(productsUrl, {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    const data = await res.json();
     console.log({ data });
-    return data;
+    return await data;
   }
 );
