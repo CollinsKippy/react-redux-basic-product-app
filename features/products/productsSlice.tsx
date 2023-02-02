e9598240592cfcd70aacda79e2ba4d4c919ba983
CollinsKippy <115981895+CollinsKippy@users.noreply.github.com>
2023-02-02 21:36:09 +0300

    1 file modified

--- features/products/productsSlice.tsx
+++ features/products/productsSlice.tsx
@@ -34,23 +34,23 @@ export const productSlice = createSlice({
   name: 'product',
   initialState,
   reducers: {
-    // loadProducts: (state, action) => {
-    //   return { ...state, isLoading: true };
-    // },
-    // loadProductsSuccess: (state, action) => {
-    //   return {
-    //     ...state,
-    //     products: action.payload,
-    //     isLoading: false,
-    //   };
-    // },
-    // loadProductsFailed: (state, action) => {
-    //   return {
-    //     ...state,
-    //     error: action.payload,
-    //     isLoading: false,
-    //   };
-    // },
+    loadProducts: (state, action) => {
+      return { ...state, isLoading: true };
+    },
+    loadProductsSuccess: (state, action) => {
+      return {
+        ...state,
+        products: action.payload,
+        isLoading: false,
+      };
+    },
+    loadProductsFailed: (state, action) => {
+      return {
+        ...state,
+        error: action.payload,
+        isLoading: false,
+      };
+    },
   },
   extraReducers: (builder) => {
     builder
@@ -69,7 +69,7 @@ export const productSlice = createSlice({
 
 export const selectProducts = (state: RootState) => state.product.products;
 
-// export const { loadProducts, loadProductsSuccess, loadProductsFailed } =
-//   productSlice.actions;
+export const { loadProducts, loadProductsSuccess, loadProductsFailed } =
+  productSlice.actions;
 
 export default productSlice.reducer;
