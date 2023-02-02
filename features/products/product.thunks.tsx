701909af0ab45b235edeaf32f63de2cb0b672339
CollinsKippy <115981895+CollinsKippy@users.noreply.github.com>
2023-02-02 20:10:44 +0300

    known bug with useDispatch in v8, could not resolve.

--- features/products/product.thunks.tsx
+++ features/products/product.thunks.tsx
@@ -1,6 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { BASE_URL } from '../../app/constants';
-// import { loadProductsFailed, loadProductsSuccess } from './product.actions';
 
 const productsUrl = BASE_URL + '/albums';
 
